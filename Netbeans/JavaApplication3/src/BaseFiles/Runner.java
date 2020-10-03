4324d6f465282a3ab1daf35c245dea2f382a3a3c
johncollins0 <johnc1999@hotmail.com>
2020-10-03 16:08:00 +1000

    Added base files from Edstem into a Netbeans project.

---
+++ Netbeans/JavaApplication3/src/BaseFiles/Runner.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BaseFiles;
+import java.util.List;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		try {
+			List<Token> results = LexicalAnalyser.analyse("public class foo { public static void main(String[] args){ int i = 0; if (i == 2) { i = i + 1; System.out.println(\"Hi\"); } else { i = i * 2; } } }");
+			System.out.println(results);
+			ParseTree tree = SyntacticAnalyser.parse(results);
+			System.out.println(tree);
+		} catch (LexicalException e) {
+			e.printStackTrace();
+		} catch (SyntaxException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
---
+++ Netbeans/JavaApplication3/src/BaseFiles/SyntacticAnalyser.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BaseFiles;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SyntacticAnalyser {
+
+	public static ParseTree parse(List<Token> tokens) throws SyntaxException {
+		//Turn the List of Tokens into a ParseTree.
+		return new ParseTree();
+	}
+
+}
